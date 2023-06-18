b02d453215bff12d58f00e29378c223f0205229a
Nayab Sunanda <104186816+sunandanayab@users.noreply.github.com>
2023-06-18 14:26:27 +0530

    Add files via upload

---
+++ UserRegisterDemo.java
@@ -0,0 +1,33 @@
+//UserRegisterDemo.java
+import java.sql.*;
+import java.util.Scanner;
+class UserRegister
+{
+void register() throws Exception
+{
+	Class.forName("oracle.jdbc.driver.OracleDriver");
+    Connection conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522:XE","system","admin");
+     //Step-1
+String dq="insert into userDet values(?,?)";
+PreparedStatement pst=conn.prepareStatement(dq);
+//Step-2
+Scanner s=new Scanner(System.in);
+System.out.println("Enter your Username");
+String uname=s.nextLine();
+System.out.println("Enter your password");
+String pwd=s.nextLine();
+pst.setString(1,uname);
+pst.setString(2,pwd);
+//Step-3
+int res=pst.executeUpdate();
+System.out.println("\t" +res+ "record is updated -----verify");
+}
+}
+class UserRegisterDemo 
+{
+	public static void main(String[] args) throws Exception
+	{
+     UserRegister ud=new UserRegister();
+         ud.register();
+	}
+}//UserRegisterDemo--ELC
