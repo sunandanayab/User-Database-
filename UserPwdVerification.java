fdf9a23d8be7a8d3b5cb4a6909c7c54b3de4d84c
Nayab Sunanda <104186816+sunandanayab@users.noreply.github.com>
2023-06-18 21:17:12 +0530

    Create UserPwdVerification.java

---
+++ UserPwdVerification.java
@@ -0,0 +1,8 @@
+class UserPwdVerification
+{
+	public static void main(String[] args) throws Exception
+	{
+      UserPwdVerify up=new UserPwdVerify();
+        up.verify();
+	}
+}//UserPwdVerification--ExecutionLogicClass
